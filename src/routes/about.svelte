67ea510a4c24cc7a659fb5f27e880ba8ac6fa08c
Vinayak Naik <vinayakanandnaik@gmail.com>
2022-04-26 23:23:15 +0530

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,16 @@
+
+<div class="about">
+  <h1>About Page</h1>
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quam minima architecto eos, eius quod itaque fugit quos aspernatur laudantium quasi labore magni necessitatibus unde veniam aut corporis quo! Reiciendis!</p>
+  <a href="/" >Home</a>
+  <a href="/guides" >View Guides</a>
+</div>
+
+  <style>
+    .about{
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+  </style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<div class="index">
+  <h1>Welcome to SvelteKit</h1>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi cumque eius molestiae a quo nobis consectetur voluptatem, libero, veritatis perspiciatis culpa maiores odio exercitationem ad accusantium aliquid atque! Quibusdam, dolorem.
+</p>
+<a href="/guides" >View Guides</a>
+<a href="/about" >About</a>
+</div>
+
+
+<style>
+  .index{
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
